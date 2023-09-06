5e248c60fb0d3a618a6c0d089ac3a2588deb8244
inmodadev3 <inmodadev3@gmail.com>
2023-09-06 13:45:59 +0000

    correccion jwt v1

--- src/controllers/v1/Usurarios_Controller.js
+++ src/controllers/v1/Usurarios_Controller.js
@@ -11,7 +11,7 @@ const validarUsuario = (req, res) => {
             console.log(err)
         }
 
-        if (rows.length !== 0) {
+        if (rows && rows.length !== 0) {
             jwt.sign({ data }, secretKet, { expiresIn: '7d' }, (err, token) => {
                 res.status(200)
                 res.json({
