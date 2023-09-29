9ec76f3aefc3ccf77f35226ad07adad788d41e3b
sistemas2 <andaoro1@gmail.com>
2023-09-29 15:09:36 -0500

    añadir si existen cambios de precios en productos

--- src/Querys/Pedidos_Querys.js
+++ src/Querys/Pedidos_Querys.js
@@ -1,7 +1,7 @@
 const { obtenerDatosDB_Hgi, obtenerDatosDb_Dash } = require('./Global_Querys')
 
-const GetPedidos_Query =async()=>{
-    return new Promise(async(resolve,reject)=>{
+const GetPedidos_Query = async () => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT intIdPedido,strIdPedidoVendedor,strNombVendedor,strNombCliente,dtFechaFinalizacion,dtFechaEnvio,intValorTotal,intEstado FROM tblPedidos order by intIdPedido desc limit 80 offset 0`
             const data = await obtenerDatosDb_Dash(query)
@@ -12,8 +12,8 @@ const GetPedidos_Query =async()=>{
     })
 }
 
-const GetPedidosNuevos_Query = async() =>{
-    return new Promise(async(resolve,reject)=>{
+const GetPedidosNuevos_Query = async () => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT intIdPedido,strIdPedidoVendedor,strNombVendedor,strNombCliente,dtFechaFinalizacion,dtFechaEnvio,intValorTotal FROM tblPedidos where intEstado = 1`
             const data = await obtenerDatosDb_Dash(query)
@@ -24,8 +24,8 @@ const GetPedidosNuevos_Query = async() =>{
     })
 }
 
-const GetPedidosEnProceso_query = async() =>{
-    return new Promise(async(resolve,reject)=>{
+const GetPedidosEnProceso_query = async () => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT intIdPedido,strIdPedidoVendedor,strNombVendedor,strNombCliente,dtFechaFinalizacion,dtFechaEnvio,intValorTotal FROM tblPedidos where intEstado in (2,3,4,5) order by intIdPedido desc limit 80 offset 0 `
             const data = await obtenerDatosDb_Dash(query)
@@ -36,8 +36,8 @@ const GetPedidosEnProceso_query = async() =>{
     })
 }
 
-const GetPedidosEnTerminal_Query = async() =>{
-    return new Promise(async(resolve,reject)=>{
+const GetPedidosEnTerminal_Query = async () => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT intIdPedido,strNombVendedor,strNombCliente,intValorTotal,dtFechaEnvio FROM tblPedidosterminal where intEstado = 1 order by intIdPedido desc`
             const data = await obtenerDatosDb_Dash(query)
@@ -48,8 +48,8 @@ const GetPedidosEnTerminal_Query = async() =>{
     })
 }
 
-const GetUbicaciones = async(strIdProducto) =>{
-    return new Promise(async(resolve,reject)=>{
+const GetUbicaciones = async (strIdProducto) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `select StrParam2 from TblProductos where StrIdProducto = '${strIdProducto}'`
             const data = await obtenerDatosDB_Hgi(query)
@@ -60,50 +60,94 @@ const GetUbicaciones = async(strIdProducto) =>{
     })
 }
 
-const GetInfoPedido_Query = async(id) =>{
-    return new Promise(async(resolve,reject)=>{
+const GetInfoPedido_Query = async (id) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT * FROM tbldetallepedidos where intIdpedido = ?`;
             const queryCabecera = `SELECT strIdCliente,strNombCliente,strCiudadCliente,strTelefonoClienteAct,dtFechaEnvio,strNombVendedor,intIdpedido,strCorreoClienteAct,strObservacion,intValorTotal FROM tblpedidos where intIdPedido = ?`;
-            let data = await obtenerDatosDb_Dash(query,[id])
-            const header = await obtenerDatosDb_Dash(queryCabecera,[id])
-            const  array_productos = []
+            let data = await obtenerDatosDb_Dash(query, [id])
+            const header = await obtenerDatosDb_Dash(queryCabecera, [id])
+            const array_productos = []
+            console.log(header[0].strIdCliente)
             for (const producto of data) {
                 let ubicaciones = await GetUbicaciones(producto.strIdProducto)
-                await array_productos.push({...producto,ubicaciones:ubicaciones[0].StrParam2
-                })
+
+                const ValidarPrecio = await ValidarPrecios_PDF(producto.strIdProducto, producto.intPrecio, header[0].strIdCliente)
+                if (ValidarPrecio) {
+                    await array_productos.push({
+                        ...producto, ubicaciones: ubicaciones[0].StrParam2, precio_cambio: true
+                    })
+                } else {
+                    await array_productos.push({
+                        ...producto, ubicaciones: ubicaciones[0].StrParam2
+                    })
+                }
+
+
             }
-            resolve({data:array_productos,header})
+            resolve({ data: array_productos, header })
         } catch (error) {
             reject(error)
         }
     })
 }
 
-const GetInfoPedidoTerminal_Query = async(id) =>{
-    return new Promise(async(resolve,reject)=>{
+const ValidarPrecios_PDF = (productoId, producto_Precio_Actual, terceroId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const query_Precio = `select IntPrecio from TblTiposTercero as TiposT 
+            inner join TblTerceros as T on TiposT.IntIdTipoTercero = T.IntTipoTercero
+            where T.StrIdTercero = '${terceroId}'`
+
+            const IntPrecio = await obtenerDatosDB_Hgi(query_Precio);
+            const obtenerPrecioProducto_query = `Select intPrecio${IntPrecio[0].IntPrecio} from tblProductos where StrIdProducto = '${productoId}'`
+            const Precio_Producto = await obtenerDatosDB_Hgi(obtenerPrecioProducto_query)
+            const objeto = Precio_Producto[0];
+
+            let valor;
+
+            for (const propiedad in objeto) {
+                if (objeto.hasOwnProperty(propiedad)) {
+                    valor = objeto[propiedad];
+                    break; // Terminar el bucle después de encontrar la primera propiedad y su valor
+                }
+            }
+
+            if (producto_Precio_Actual !== valor) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+const GetInfoPedidoTerminal_Query = async (id) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT * FROM tbldetallepedidosterminal where intIdpedido = ?`;
             const queryCabecera = `SELECT strIdCliente,strNombCliente,strCiudadCliente,strTelefonoClienteAct,dtFechaEnvio,strNombVendedor,intIdpedido FROM tblpedidosterminal where intIdPedido = ?`;
-            const rows = await obtenerDatosDb_Dash(query,[id])
-            const header = await obtenerDatosDb_Dash(queryCabecera,[id])
-    
+            const rows = await obtenerDatosDb_Dash(query, [id])
+            const header = await obtenerDatosDb_Dash(queryCabecera, [id])
+
             let detallePedido = [];
-    
+
             for (const item of rows) {
                 let ubicaciones = await GetUbicaciones(item.strIdProducto);
                 detallePedido.push({ ...item, ubicaciones: ubicaciones[0].StrParam2 });
             }
-            
-            resolve({detallePedido,header})
+
+            resolve({ detallePedido, header })
         } catch (error) {
             reject(error)
         }
     })
 }
 
-const GetPedidoXId_Query = async(id) =>{
-    return new Promise(async(resolve,reject)=>{
+const GetPedidoXId_Query = async (id) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `SELECT intIdPedido,strIdPedidoVendedor,strNombVendedor,strNombCliente,dtFechaFinalizacion,dtFechaEnvio,intValorTotal,intEstado FROM tblPedidos where intIdPedido like '${id}%' order by intIdPedido desc limit 80 offset 0`
 
@@ -115,11 +159,11 @@ const GetPedidoXId_Query = async(id) =>{
     })
 }
 
-const PutEstadoPedido_Query = async(id,estado) =>{
-    return new Promise(async(resolve,reject)=>{
+const PutEstadoPedido_Query = async (id, estado) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const query = `UPDATE tblpedidos SET intEstado = ? where intIdPedido = ?`
-            await obtenerDatosDb_Dash(query,[estado,id])
+            await obtenerDatosDb_Dash(query, [estado, id])
             resolve(1)
         } catch (error) {
             reject(error)
