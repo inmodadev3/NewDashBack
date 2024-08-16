8bdfd3fffcaff05a6301df98fd8e755156dcbea9
sistemas2 <andaoro1@gmail.com>
2024-08-16 12:54:51 -0500

    Actualizar estado posible cliente

--- src/routes/v1/posibles_clientes.js
+++ src/routes/v1/posibles_clientes.js
@@ -1,4 +1,4 @@
-const { CrearPosibleCliente_Controller, ActualizarInformacionPosibleCliente_Controller, ConsultarXEstado_Controller, ConsultarXBusqueda_Controller } = require('../../controllers/v1/PosiblesClientes/PosiblesClientes_Controller')
+const { CrearPosibleCliente_Controller, ActualizarInformacionPosibleCliente_Controller, ConsultarXEstado_Controller, ConsultarXBusqueda_Controller, ActualizarEstado_Controller } = require('../../controllers/v1/PosiblesClientes/PosiblesClientes_Controller')
 
 const route = require('express').Router()
 
@@ -6,6 +6,7 @@ route.post('/', CrearPosibleCliente_Controller)
 route.put('/', ActualizarInformacionPosibleCliente_Controller)
 route.get('/', ConsultarXEstado_Controller)
 route.get('/:dato', ConsultarXBusqueda_Controller)
+route.put('/:id', ActualizarEstado_Controller)
 
 
 
